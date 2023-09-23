8d24b18e4748b88a1c8d2e8011c688c7f6bf99f3
Ronald Sancho <ronalsancho@gmail.com>
2023-09-23 12:39:30 -0600

    Modify styles in My Skills

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ var typed = new Typed(".Rol", {
 })
 
 //Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
+function skills(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        let skill = document.getElementsByClassName("progress");
+        skill[0].classList.add("HTML");
+        skill[1].classList.add("CSS");
+        skill[2].classList.add("JS");
+        skill[3].classList.add("NODEJS");
+        skill[4].classList.add("NETCORE");
+        skill[5].classList.add("communication");
+        skill[6].classList.add("responsibility");
+        skill[7].classList.add("teamwork");
+        skill[8].classList.add("adaptability");
+        skill[9].classList.add("problems");
     }
 }
 
 window.onscroll = function(){
-    efectoHabilidades();
+    skills();
 } 
